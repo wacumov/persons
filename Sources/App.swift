0aa0ea6e44d0deb5397c28c06083e6a419a75660
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2025-02-06 22:30:24 +0200

    Add empty app

---
+++ Sources/App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct PersonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ Sources/Views/Screens/PersonListView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct PersonListView: View {
+    var body: some View {
+        Text("PersonListView")
+    }
+}
+
+#if DEBUG
+#Preview {
+    PersonListView()
+}
+#endif
